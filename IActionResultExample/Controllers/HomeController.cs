9178fac919907a81d051c7dc66b82f37ca33a31c
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-25 15:33:20 +0100

    Query String vs Route Data

--- IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/Controllers/HomeController.cs
@@ -4,68 +4,34 @@ namespace IActionResultExample.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("bookstore")]
-        public IActionResult Index()
+        [Route("bookstore/{bookid?}/{isloggedin?}")]
+        public IActionResult Index(int? bookid, bool? isloggedin)
         {
             // Book id should be applied
-            if (!Request.Query.ContainsKey("bookid"))
+            // Url: /bookstore?bookid=10&isLoggedin=true
+            if (bookid.HasValue == false)
             {
                 // return new BadRequestResult();
-                return BadRequest("Book id is not supplied");
+                return BadRequest("Book id is not supplied or empty");
             }
 
-            // Book id can't be empty
-            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
+            // Book id can't be less than or equal to 0
+            if (bookid <= 0)
             {
-                return BadRequest("Book id can't be null or empty");
+                return BadRequest("Book id can't be less than or equal to 0");
             }
 
-            // Book id should be between 1 to 1000
-            int bookId = Convert.ToInt16(ControllerContext.HttpContext.Request.Query["bookid"]);
-
-            if (bookId <= 0)
-            {
-                return BadRequest("Book id can't be less then or equal to zero");
-            }
-
-            if (bookId > 1000)
+            if (bookid > 1000)
             {
                 return NotFound("Book id can't be greater than 1000");
             }
 
-            // isLoggedin should be true
-            if (Convert.ToBoolean(Request.Query["isLoggedin"]) == false)
+            if (isloggedin == false)
             {
-                // return Unauthorized("User must be authenticated");
                 return StatusCode(401);
             }
 
-            // return File("/sample.pdf", "application/pdf");
-
-            // 302 - Found - RedirectToActionResult
-            // return new RedirectToActionResult("Books", "Store", new { id = bookId });
-            // 302 - Found
-            // return RedirectToAction("Books", "Store", new { id = bookId });
-
-            // 301 - Moved Permanently - RedirectToActionResult
-            // return new RedirectToActionResult("Books", "Store", new { }, true);
-            // 301 - Moved Permanently
-            // return RedirectToActionPermanent("Books", "Store", new { id = bookId });
-
-            // 302 - Found - LocalRedirectResult
-            // return new LocalRedirectResult($"store/books/{bookId}");
-            // 302 - Found
-            // return LocalRedirect($"store/books/{bookId}");
-
-            // 301 - Moved Permanently - LocalRedirectResult
-            // return new LocalRedirectResult($"store/books/{bookId}", true);
-            // 301 - Moved Permanently
-            // return LocalRedirect($"store/books/{bookId}");
-
-            // 302 - Found
-            // return Redirect($"store/books/{bookId}");
-            // 301 - Moved Permanently
-            return RedirectPermanent($"store/books/{bookId}");
+            return Content($"Book id: {bookid}", "text/plain");
         }
     }
 }
