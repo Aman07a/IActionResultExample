2020b00092f7b9bd0a5e6d1a6f27d2ebd84b5cd3
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-25 09:34:32 +0100

    Status Code Results

--- IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/Controllers/HomeController.cs
@@ -10,15 +10,14 @@ namespace IActionResultExample.Controllers
             // Book id should be applied
             if (!Request.Query.ContainsKey("bookid"))
             {
-                Response.StatusCode = 400;
-                return Content("Book id is not supplied");
+                // return new BadRequestResult();
+                return BadRequest("Book id is not supplied");
             }
 
             // Book id can't be empty
             if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
             {
-                Response.StatusCode = 400;
-                return Content("Book id can't be null or empty");
+                return BadRequest("Book id can't be null or empty");
             }
 
             // Book id should be between 1 to 1000
@@ -26,21 +25,19 @@ namespace IActionResultExample.Controllers
 
             if (bookId <= 0)
             {
-                Response.StatusCode = 400;
-                return Content("Book id can't be less then or equal to zero");
-            }
-
+                return BadRequest("Book id can't be less then or equal to zero");
+            } 
+            
             if (bookId > 1000)
             {
-                Response.StatusCode = 400;
-                return Content("Book id can't be greater than 1000");
+                return NotFound("Book id can't be greater than 1000");
             }
 
             // isLoggedin should be true
             if (Convert.ToBoolean(Request.Query["isLoggedin"]) == false)
             {
-                Response.StatusCode = 401;
-                return Content("User must be authenticated");
+                // return Unauthorized("User must be authenticated");
+                return StatusCode(401);
             }
 
             return File("/sample.pdf", "application/pdf");
